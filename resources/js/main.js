e1b153b65e9761af1415c80ac054e9d2b0262cca
Kim <kkubik7@hotmail.com>
2021-09-22 23:09:17 -0500

    initial

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+
+// If user clicks on button, we want what they typed to the 'todo' list
+document.getElementById("addTask").addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (value){
+
+        /*todo error check for if the list is empty*/
+
+    }
+
+});
